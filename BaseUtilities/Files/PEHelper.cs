a02b709da21c0d8990d6eea6da947087831e1c0a
worksWithTools <sa_upton@hotmail.com>
2019-06-09 15:30:21 +0100

    Added PEHelper
    To determine if a DLL file is native or managed

---
+++ BaseUtilities/Files/PEHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+public static class PortableExecutableHelper
+{
+    public static bool IsDotNetAssembly(string peFile)
+    {
+        try
+        {
+            System.Reflection.AssemblyName testAssembly =
+                System.Reflection.AssemblyName.GetAssemblyName(peFile);
+
+            return true;
+        }
+
+        catch (System.IO.FileNotFoundException)
+        {
+            System.Console.WriteLine("The file cannot be found.");
+        }
+
+        catch (System.BadImageFormatException)
+        {
+            System.Console.WriteLine("The file is not an assembly.");
+        }
+
+        return false;
+    }
+}

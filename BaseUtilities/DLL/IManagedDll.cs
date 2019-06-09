ed2089d62f2996861c37dfc5189c79f4d87ccf10
worksWithTools <sa_upton@hotmail.com>
2019-06-09 18:32:30 +0100

    Moved the DLL interfaces into BaseUtils dll
    ...though the namespace is the same for now

---
+++ BaseUtilities/DLL/IManagedDll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static EDDiscovery.DLL.EDDDLLIF;
+
+namespace EDDiscovery.DLL
+{
+    public interface IManagedDll
+    {
+        String EDDInitialise(string vstr,
+                             string dllfolder,
+                             EDDCallBacks callbacks);
+
+        void EDDRefresh(string cmdname, JournalEntry lastje);
+
+        void EDDNewJournalEntry(JournalEntry nje);
+
+        void EDDTerminate();
+
+        String EDDActionCommand(string cmdname, string[] paras);
+
+        void EDDActionJournalEntry(JournalEntry lastje);
+    }
+}
